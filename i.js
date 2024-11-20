e0d631cdcc609c14ecfb189046e9445ac179c9db
Jasper Hu <wenjia.hu@duke.edu>
2024-11-20 16:37:00 -0500

    g

--- i.js
+++ i.js
@@ -29,56 +29,5 @@ db.committees.aggregate([
         }
       }
     },
-    { $unwind: "$members" },
-    {
-      $addFields: {
-        party: {
-          $arrayElemAt: [
-            "$members.roles.party",
-            0
-          ]
-        }
-      }
-    },
-    // Group by committee and party to count members
-    {
-      $group: {
-        _id: {
-          committee: "$committee",
-          party: "$party"
-        },
-        count: { $sum: 1 }
-      }
-    },
-    // Reshape to aggregate counts into Democrat, Republican, and Independent
-    {
-      $group: {
-        _id: "$_id.committee",
-        Democrat: {
-          $sum: {
-            $cond: [{ $eq: ["$_id.party", "Democrat"] }, "$count", 0]
-          }
-        },
-        Republican: {
-          $sum: {
-            $cond: [{ $eq: ["$_id.party", "Republican"] }, "$count", 0]
-          }
-        },
-        Independent: {
-          $sum: {
-            $cond: [{ $eq: ["$_id.party", "Independent"] }, "$count", 0]
-          }
-        }
-      }
-    },
-    {
-      $project: {
-        _id: 0,
-        committee: "$_id",
-        Democrat: 1,
-        Republican: 1,
-        Independent: 1
-      }
-    }
   ]);
   
