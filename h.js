1e4f423f211c1581c8d5b9273553ce8d8dedb902
Jasper Hu <wenjia.hu@duke.edu>
2024-11-20 16:02:06 -0500

    g

--- h.js
+++ h.js
@@ -1,7 +1,6 @@
 printjson(db.people.aggregate([
     {
         $match: {
-            displayname: "House Select Committee on the Strategic Competition Between the United States and the Chinese Communist Party",
         }
     }
 ]).toArray());
