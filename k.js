1d3c985e39c7b68257ec726f50ee2fdb8f60bdb1
Jasper Hu <wenjia.hu@duke.edu>
2024-11-20 17:20:57 -0500

    g

--- k.js
+++ k.js
@@ -1,5 +1,4 @@
 printjson(db.people.aggregate([
-
     {
       $match: {
         gender: "F",
@@ -7,46 +6,36 @@ printjson(db.people.aggregate([
         "roles.state": "CA"
       }
     },
-
     {
       $lookup: {
         from: "committees",
-        localField: "_id",
+        localField: "_id", // Match legislator ID in people with members.id in committees
         foreignField: "members.id",
         as: "committee_info"
       }
     },
     {
-      $addFields: {
-        subcommittee_members: {
-          $reduce: {
-            input: "$committee_info.subcommittees",
-            initialValue: [],
-            in: { $concatArrays: ["$$value", "$$this.members"] }
-          }
-        }
+      $lookup: {
+        from: "committees",
+        let: { personId: "$_id" },
+        pipeline: [
+          { $unwind: "$subcommittees" }, // Unwind subcommittees
+          { $unwind: "$subcommittees.members" }, // Unwind subcommittee members
+          { $match: { $expr: { $eq: ["$$personId", "$subcommittees.members.id"] } } }
+        ],
+        as: "subcommittee_info"
       }
     },
-    {
-      $addFields: {
-        all_committee_members: {
-          $concatArrays: [
-            { $arrayElemAt: ["$committee_info.members", 0] },
-            "$subcommittee_members"
-          ]
-        }
-      }
-    },
-
     {
       $match: {
-        all_committee_members: { $not: { $elemMatch: { id: "$_id" } } }
+        committee_info: { $size: 0 },
+        subcommittee_info: { $size: 0 } 
       }
     },
     {
       $project: {
-        _id: 0,
-        name: "$name" 
+        _id: 0, // Exclude _id from the output
+        name: "$name" // Include only the name field
       }
     }
   ]).toArray());
